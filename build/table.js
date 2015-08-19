1e8e810059f9a7721f2876f4412622e11ad75cf7
JarvanYang <510223397@qq.com>
2015-08-19 15:03:16 +0800

    add html2js

---
+++ build/table.js
@@ -0,0 +1,51 @@
+(function(module) {
+try {
+  module = angular.module('newbeeTable');
+} catch (e) {
+  module = angular.module('newbeeTable', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('srctable.html',
+    '<div>\n' +
+    '\n' +
+    '  <div class="top-left">\n' +
+    '      <div ng-repeat="con in fixedConfigs" class="fix-col-{{$index}}">{{con.label}}</div>\n' +
+    '\n' +
+    '  </div>\n' +
+    '  <div class="top">\n' +
+    '    <div class="panel">\n' +
+    '      <div ng-repeat="con in normalConfigs" class="nor-col-{{$index}}">{{con.label}}</div>\n' +
+    '    </div>\n' +
+    '\n' +
+    '\n' +
+    '  </div>\n' +
+    '\n' +
+    '  <div class="left">\n' +
+    '    <div class="panel">\n' +
+    '      <div  ng-repeat="row in data">\n' +
+    '        <div ng-repeat="con in fixedConfigs" class="fix-col-{{$index}}">\n' +
+    '          {{row[con.key]}}\n' +
+    '        </div>\n' +
+    '      </div>\n' +
+    '\n' +
+    '    </div>\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    '  </div>\n' +
+    '  <div class="main">\n' +
+    '    <div class="panel">\n' +
+    '\n' +
+    '\n' +
+    '    <div  ng-repeat="row in data">\n' +
+    '      <div ng-repeat="con in normalConfigs" class="nor-col-{{$index}}">\n' +
+    '        {{row[con.key]}}\n' +
+    '      </div>\n' +
+    '    </div>\n' +
+    '      </div>\n' +
+    '  </div>\n' +
+    '</div>\n' +
+    '');
+}]);
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var coffeelint = require('gulp-coffeelint');
-
+var ngHtml2Js = require("gulp-ng-html2js");
+var concat = require("gulp-concat");
 var source = {
 
   js: {
@@ -17,6 +18,18 @@ var source = {
 };
 var  build ='build';
 
+
+gulp.task('template', function () {
+  gulp.src("./src/*.html")
+      .pipe(ngHtml2Js({
+          moduleName: "newbeeTable",
+                  prefix: "src"
+      }))
+      .pipe(gulp.dest(build));
+});
+
+
+
 gulp.task('validate_coffee', function () {
   gulp.src(source.js.app)
     .pipe(coffeelint())
@@ -30,6 +43,10 @@ gulp.task('compile_coffee', ['validate_coffee'], function() {
     .pipe(gulp.dest(build));
 });
 
+
+
+
+
 gulp.task('css', function() {
   gulp.src("src/style.css")
     .pipe(gulp.dest(build));
@@ -39,6 +56,14 @@ gulp.task('watch', function () {
     gulp.watch(source.js.app, ['compile_coffee','css']);
 });
 
-gulp.task('default',['compile_coffee','css','watch'], function() {
+gulp.task('default',['compile_coffee','css','watch','template'], function() {
   // place code for your default task here
+});
+
+gulp.task("dist", function() {
+
+  gulp.src(['build/directive.js','build/table.js'])
+    .pipe(concat("newbeeTable.min.js"))
+    .pipe(gulp.dest("./dist"));
+
 });
