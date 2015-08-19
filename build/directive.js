bb039bc2816e8950b332c54009afe8caa1b0683d
JarvanYang <510223397@qq.com>
2015-08-19 17:23:45 +0800

    12

--- build/directive.js
+++ build/directive.js
@@ -15,63 +15,73 @@ newbeeTable = function($window, $timeout) {
       height = $window.innerHeight - tEle.offset().top - 60;
       tEle.css('height', height + 'px');
       return function(scope, ele) {
-        var c;
-        scope.fixedConfigs = (function() {
-          var j, len, ref, results;
-          ref = scope.config;
-          results = [];
-          for (j = 0, len = ref.length; j < len; j++) {
-            c = ref[j];
-            if (c.isFixed) {
-              results.push(c);
+        var init, leftPanel, mainPanel, sl, st, topPanel;
+        init = function() {
+          var c;
+          scope.fixedConfigs = (function() {
+            var j, len, ref, results;
+            ref = scope.config;
+            results = [];
+            for (j = 0, len = ref.length; j < len; j++) {
+              c = ref[j];
+              if (c.isFixed) {
+                results.push(c);
+              }
             }
-          }
-          return results;
-        })();
-        scope.normalConfigs = (function() {
-          var j, len, ref, results;
-          ref = scope.config;
-          results = [];
-          for (j = 0, len = ref.length; j < len; j++) {
-            c = ref[j];
-            if (!c.isFixed) {
-              results.push(c);
+            return results;
+          })();
+          scope.normalConfigs = (function() {
+            var j, len, ref, results;
+            ref = scope.config;
+            results = [];
+            for (j = 0, len = ref.length; j < len; j++) {
+              c = ref[j];
+              if (!c.isFixed) {
+                results.push(c);
+              }
             }
-          }
-          return results;
-        })();
-        return $timeout(function() {
-          var e, fix, i, j, k, leftPanel, len, len1, mainPanel, nor, ref, ref1, sl, st, topPanel;
-          ref = scope.fixedConfigs;
-          for (i = j = 0, len = ref.length; j < len; i = ++j) {
-            fix = ref[i];
-            e = ele.find('.fix-col-' + i);
-            e.width(e.maxWidth());
-          }
-          ref1 = scope.normalConfigs;
-          for (i = k = 0, len1 = ref1.length; k < len1; i = ++k) {
-            nor = ref1[i];
-            e = ele.find('.nor-col-' + i);
-            e.width(e.maxWidth());
-          }
-          mainPanel = ele.find('div.main');
-          topPanel = ele.find('div.top>div.panel');
-          leftPanel = ele.find('div.left>div.panel');
-          sl = 0;
-          st = 0;
-          return mainPanel.scroll(function() {
-            var scrollLeft, scrollTop;
-            scrollLeft = mainPanel.scrollLeft();
-            scrollTop = mainPanel.scrollTop();
-            if (sl !== scrollLeft) {
-              topPanel.css('left', -scrollLeft);
-              sl = scrollLeft;
+            return results;
+          })();
+          return $timeout(function() {
+            var e, fix, i, j, k, len, len1, nor, ref, ref1, results;
+            ref = scope.fixedConfigs;
+            for (i = j = 0, len = ref.length; j < len; i = ++j) {
+              fix = ref[i];
+              e = ele.find('.fix-col-' + i);
+              e.width(e.maxWidth());
             }
-            if (st !== scrollTop) {
-              leftPanel.css('top', -scrollTop);
-              return st = scrollTop;
+            ref1 = scope.normalConfigs;
+            results = [];
+            for (i = k = 0, len1 = ref1.length; k < len1; i = ++k) {
+              nor = ref1[i];
+              e = ele.find('.nor-col-' + i);
+              results.push(e.width(e.maxWidth()));
             }
+            return results;
           });
+        };
+        scope.$watch('config', function(config) {
+          if (config) {
+            return init();
+          }
+        }, true);
+        mainPanel = ele.find('div.main');
+        topPanel = ele.find('div.top>div.panel');
+        leftPanel = ele.find('div.left>div.panel');
+        sl = 0;
+        st = 0;
+        return mainPanel.scroll(function() {
+          var scrollLeft, scrollTop;
+          scrollLeft = mainPanel.scrollLeft();
+          scrollTop = mainPanel.scrollTop();
+          if (sl !== scrollLeft) {
+            topPanel.css('left', -scrollLeft);
+            sl = scrollLeft;
+          }
+          if (st !== scrollTop) {
+            leftPanel.css('top', -scrollTop);
+            return st = scrollTop;
+          }
         });
       };
     }
--- src/directive.coffee
+++ src/directive.coffee
@@ -11,33 +11,40 @@ newbeeTable = ($window,$timeout) ->
   compile:(tEle,tAttr)->
     height=$window.innerHeight-tEle.offset().top-60
     tEle.css('height',height+'px')
-    
+
     (scope,ele) ->
+      init=->
+        scope.fixedConfigs = (c for c in scope.config when c.isFixed)
+        scope.normalConfigs = (c for c in scope.config when !c.isFixed)
+        $timeout ->
+          for fix,i in scope.fixedConfigs
+            e=ele.find('.fix-col-'+i)
+            e.width(e.maxWidth())
+          for nor,i in scope.normalConfigs
+            e=ele.find('.nor-col-'+i)
+            e.width(e.maxWidth())
 
-      scope.fixedConfigs = (c for c in scope.config when c.isFixed)
-      scope.normalConfigs = (c for c in scope.config when !c.isFixed)
-      $timeout ->
-        for fix,i in scope.fixedConfigs
-          e=ele.find('.fix-col-'+i)
-          e.width(e.maxWidth())
-        for nor,i in scope.normalConfigs
-          e=ele.find('.nor-col-'+i)
-          e.width(e.maxWidth())
-        mainPanel = ele.find('div.main')
-        topPanel = ele.find('div.top>div.panel')
-        leftPanel=ele.find('div.left>div.panel')
-        sl=0
-        st=0
-        mainPanel.scroll ()->
-          scrollLeft=mainPanel.scrollLeft()
-          scrollTop=mainPanel.scrollTop()
-          unless sl is scrollLeft
-            topPanel.css 'left',-scrollLeft
-            sl=scrollLeft
 
-          unless st is scrollTop
-            leftPanel.css 'top',-scrollTop
-            st=scrollTop
+      scope.$watch('config',(config)->
+
+        init() if config
+      true)
+
+      mainPanel = ele.find('div.main')
+      topPanel = ele.find('div.top>div.panel')
+      leftPanel=ele.find('div.left>div.panel')
+      sl=0
+      st=0
+      mainPanel.scroll ()->
+        scrollLeft=mainPanel.scrollLeft()
+        scrollTop=mainPanel.scrollTop()
+        unless sl is scrollLeft
+          topPanel.css 'left',-scrollLeft
+          sl=scrollLeft
+
+        unless st is scrollTop
+          leftPanel.css 'top',-scrollTop
+          st=scrollTop
 
 
 
